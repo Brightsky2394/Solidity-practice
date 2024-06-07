a66387d77a33e9e7e65fb80681731fb9921d646d
skycode <sikiruyaya1@gmail.com>
2024-06-07 13:52:38 +0100

    proxy contract delegating call to original contract

---
+++ Proxy.sol
@@ -0,0 +1,30 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract OriginalContract {
+    uint256 public num;
+    address public sender;
+    uint256 public value;
+
+    function setVariable(uint256 _num) public payable {
+        num = _num;
+        sender = msg.sender;
+        value = msg.value;
+    }
+}
+
+contract Proxy {
+    uint256 public num;
+    address public sender;
+    uint256 public value;
+
+    function setVariable(
+        address _originalContract,
+        uint256 _num
+    ) public payable {
+        (bool success, ) = _originalContract.delegatecall(
+            abi.encodeWithSignature("setVariable(uint256)", _num)
+        );
+        require(success, "Delegate call failed");
+    }
+}
