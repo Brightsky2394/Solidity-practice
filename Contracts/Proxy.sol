343408d719f250206e1f2773a62ee1d40ade6366
skycode <sikiruyaya1@gmail.com>
2024-07-21 12:39:04 +0100

    proxy contract

--- Contracts/Proxy.sol
+++ Contracts/Proxy.sol
@@ -1,30 +1,71 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-contract OriginalContract {
-    uint256 public num;
-    address public sender;
-    uint256 public value;
+contract TestContract1 {
+    address public owner;
 
-    function setVariable(uint256 _num) public payable {
-        num = _num;
-        sender = msg.sender;
-        value = msg.value;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner != msg.sender, "Not the owner");
+        _;
+    }
+
+    function setOwner(address _owner) external onlyOwner {
+        owner = _owner;
+    }
+}
+
+contract TestContract2 {
+    address public owner = msg.sender;
+    uint public value = msg.value;
+    uint public x;
+    uint public y;
+
+    constructor(uint _x, uint _y) payable {
+        x = _x;
+        y = _y;
     }
 }
 
 contract Proxy {
-    uint256 public num;
-    address public sender;
-    uint256 public value;
+    event Deploy(address);
 
-    function setVariable(
-        address _originalContract,
-        uint256 _num
-    ) public payable {
-        (bool success, ) = _originalContract.delegatecall(
-            abi.encodeWithSignature("setVariable(uint256)", _num)
-        );
-        require(success, "Delegate call failed");
+    fallback() external {}
+
+    function deploy(bytes memory _code) external payable {
+        address addr;
+        assembly {
+            addr := create(callvalue(), add(_code, 0x20), mload(_code))
+        }
+        require(addr != address(0), "Deployed failed");
+        emit Deploy(addr);
+    }
+
+    function execute(address _target, bytes memory _data) external payable {
+        (bool success, ) = _target.call{value: msg.value}(_data);
+        require(success, "Execution failed");
+    }
+}
+
+contract Helper {
+    function getBytecode1() external pure returns (bytes memory byteCode) {
+        byteCode = type(TestContract1).creationCode;
+        return byteCode;
+    }
+
+    function getBytecode2(
+        uint _x,
+        uint _y
+    ) external pure returns (bytes memory byteCode) {
+        byteCode = type(TestContract2).creationCode;
+        return abi.encodePacked(byteCode, abi.encode(_x, _y));
+    }
+
+    function getCalldata(address _owner) external pure returns () {
+        return abi.encodeWithSignature("setOwner(address)", _owner);
     }
 }
